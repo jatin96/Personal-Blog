36dd529b351cc7bbd124892f733e90b2fac2930e
jatin.jt.narula <jatin.jt.narula@gmail.com>
2020-03-22 20:20:57 +0530

    adding graphql for fetching post data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,50 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, StaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { graphql,staticQuery } from 'gatsby';
+import Post from '../components/Post'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home"  keywords={[`gatsby`, `application`, `react`]}/>
     <h1>home page</h1>
+    <StaticQuery 
+    query={indexQuery} 
+    render={data => {
+      return (
+        <div>
+          {data.allMarkdownRemark.edges.map(({ node }) => (
+            <Post title={node.frontmatter.title}
+            author={node.frontmatter.author}
+            date={node.frontmatter.date}
+            path={node.frontmatter.path}
+            body={node.frontmatter.body}/>
+          ))}
+        </div>
+      )
+    }} />
   </Layout>
 )
+
+const indexQuery = graphql`
+query {
+  allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC}) {
+    edges {
+      node {
+        id
+        frontmatter {
+          title
+          date(formatString: "MMM Do YYYY")
+          author
+          path
+        }
+        excerpt
+      }
+    }
+  }
+}
+`
 
 export default IndexPage
---
+++ src/pages/posts/003-third-post.md
@@ -0,0 +1,8 @@
+---
+title: 'Third Post - Introduction'
+date: 2020-03-22 9:00:00
+author: jatin
+path: '/third-post'
+---
+
+Jatin Narula's personal blog siteJatin Narula's personal blog siteJatin Narula's personal blog siteJatin Narula's personal blog siteJatin Narula's personal blog siteJatin Narula's personal blog siteJatin Narula's personal blog siteJatin Narula's personal blog siteJatin Narula's personal blog siteJatin Narula's personal blog siteJatin Narula's personal blog siteJatin Narula's personal blog siteJatin Narula's personal blog siteJatin Narula's personal blog site
