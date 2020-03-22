8cef948dc98662eb34335997f570789e9180c325
jatin.jt.narula <jatin.jt.narula@gmail.com>
2020-03-22 17:34:10 +0530

    added navbar and styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,53 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText
+} from 'reactstrap';
+import React, { Component } from 'react';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen : false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen : false
+    });
+  }
+  render() {
+    return (
+      <div>
+      <Navbar fixed = "top" color="light" light expand="sm">
+        <NavbarBrand href="/">{this.props.siteTitle}</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/team">Team</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/tags">Tags</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/about">About</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
     </div>
-  </header>
-)
+    );
+  }
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
