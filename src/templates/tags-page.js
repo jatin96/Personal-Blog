3b83ec88a003d4a0d4e6d7e003bfb5b993aa5e2a
jatin.jt.narula <jatin.jt.narula@gmail.com>
2020-04-17 04:07:20 +0530

    fixing the tab-page

--- src/templates/tags-page.js
+++ src/templates/tags-page.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Layout from '../components/layout'
-import SEO from '../components/layout'
+import SEO from '../components/seo'
 import {Button, Badge} from 'reactstrap'
 import {slugify} from '../utility/utilityFunction'
 
-const tagsPage = ({pageContext}) => {
-    const {tags, tagsPostCount} = pageContext;
+const tagsPage = ({ pageContext }) => {
+    const {tags, tagsPostCount} = pageContext
     return (
         <Layout pageTitle="App Tags">
-            <SEO title="All tags"></SEO>
+            <SEO title="All tags" keywords={['tags','topics']}></SEO>
             <ul>
                 {tags.map(tag => (
                     <li key={tag} style={{ marginBottom: '10px'}}><Button color="primary" href={`/tag/${slugify(tag)}`}>
